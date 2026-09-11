0372715422a355359754ec26180dad42c7b0fc60
Ken Caruso <87687+ipl31@users.noreply.github.com>
2026-09-11 14:30:04 -0700

    Add verified issue 53 playlist mappings

--- BotOrNot.Tests/PlaylistMappingTests.cs
+++ BotOrNot.Tests/PlaylistMappingTests.cs
@@ -1,4 +1,6 @@
 using BotOrNot.Core.Services;
+using System.Reflection;
+using System.Text.Json;
 
 namespace BotOrNot.Tests;
 
@@ -9,6 +11,12 @@ public class PlaylistMappingTests
     [TestCase("Playlist_RopeSmileDuo", "Reload Build - Duos")]
     [TestCase("Playlist_Habanero_JumpBear_Duos", "Ranked Reload Build - Duos")]
     [TestCase("Playlist_HabaneroDuo", "Ranked BR Build - Duos")]
+    [TestCase("Playlist_ForbiddenFruitOldNoBuildBRSolo", "Blitz Zero Build - Solo")]
+    [TestCase("Playlist_MatchMistSolo", "Reload Build - Solo")]
+    [TestCase("Playlist_MatchMistDuo", "Reload Build - Duos")]
+    [TestCase("Playlist_MatchMistSquad", "Reload Build - Squads")]
+    [TestCase("Playlist_NoBuildBR_Habanero_Solo", "Ranked Zero Build - Solo")]
+    [TestCase("Playlist_Trios", "BR Build - Trios")]
     public void NewlyObservedPlaylistsHaveFamilyAndTeamNames(string playlist, string expectedDisplayName)
     {
         Assert.That(PlaylistHelper.GetDisplayName(playlist), Is.EqualTo(expectedDisplayName));
@@ -34,5 +42,31 @@ public class PlaylistMappingTests
         Assert.That(
             PlaylistHelper.GetDisplayNameWithFallback(unknownPlaylist),
             Is.EqualTo(unknownPlaylist));
+    }
+
+    [TestCase("Playlist_ForbiddenFruitOldNoBuildBRSolo", "Blitz Zero Build - Solo")]
+    [TestCase("Playlist_MatchMistSolo", "Reload Build - Solo")]
+    [TestCase("Playlist_MatchMistDuo", "Reload Build - Duos")]
+    [TestCase("Playlist_MatchMistSquad", "Reload Build - Squads")]
+    [TestCase("Playlist_NoBuildBR_Habanero_Solo", "Ranked Zero Build - Solo")]
+    [TestCase("Playlist_Trios", "BR Build - Trios")]
+    public void Issue53PlaylistsAreCaseInsensitive(string playlist, string expectedDisplayName)
+    {
+        Assert.That(PlaylistHelper.GetDisplayName(playlist.ToLowerInvariant()), Is.EqualTo(expectedDisplayName));
+        Assert.That(PlaylistHelper.GetDisplayName(playlist.ToUpperInvariant()), Is.EqualTo(expectedDisplayName));
+    }
+
+    [Test]
+    public void PlaylistMappingKeysAreUniqueIgnoringCase()
+    {
+        var assembly = typeof(PlaylistHelper).Assembly;
+        using var stream = assembly.GetManifestResourceStream("BotOrNot.Core.Data.PlaylistMappings.json");
+        Assert.That(stream, Is.Not.Null);
+        using var document = JsonDocument.Parse(stream!);
+        var names = document.RootElement.GetProperty("playlists").EnumerateArray()
+            .Select(item => item.GetProperty("playlist_name").GetString()!.Trim().ToUpperInvariant())
+            .ToList();
+
+        Assert.That(names, Is.EqualTo(names.Distinct().ToList()));
     }
 }
